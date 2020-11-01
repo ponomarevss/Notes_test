6e6e7fc6dc8b4fad8f6955b07ec4e96c0b94ec61
ponomarevss <s-siberia@yandex.ru>
2020-11-01 23:01:52 +0500

    SplashViewModel.kt - дописал liveData самооткручивающимся обзервером

--- app/src/main/java/com/example/notes/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/notes/ui/splash/SplashViewModel.kt
@@ -1,17 +1,28 @@
 package com.example.notes.ui.splash
 
 import android.provider.ContactsContract
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import com.example.notes.data.NotesRepository
+import com.example.notes.data.entity.User
 import com.example.notes.data.errors.NoAuthException
 import com.example.notes.ui.base.BaseViewModel
 
 class SplashViewModel: BaseViewModel<Boolean?, SplashViewState>() {
 
-    fun requestUser() {
-        NotesRepository.getCurrentUser().observeForever {
-            viewStateLiveData.value = it?.let { SplashViewState(true) } ?: let {
+    private var userLiveData : LiveData<User?>? = null
+    private val userObserver = object : Observer<User?> {
+        override fun onChanged(t: User?) {
+            viewStateLiveData.value = t?.let { SplashViewState(true) } ?: let {
                 SplashViewState(error = NoAuthException())
             }
+            userLiveData?.removeObserver(this)
         }
+
+    }
+
+    fun requestUser() {
+        userLiveData = NotesRepository.getCurrentUser()
+        userLiveData?.observeForever(userObserver)
     }
 }
